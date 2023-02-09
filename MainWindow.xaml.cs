c4e3788365e321f8782fc035836487ca662b03c0
Michael Zipp <milonti1@gmail.com>
2023-02-09 11:53:21 -0500

    Commit D while I fix C

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace CompassInterviewTest
     public partial class MainWindow : Window
     {
         private MainWindowViewModel _viewModel;
-        
+
         public MainWindow()
         {
             _viewModel = new MainWindowViewModel();
@@ -39,7 +39,12 @@ namespace CompassInterviewTest
         {
             //Todo B. Modify this variable assignment to calculate the intensity of the light using OpacityDistanceFunction based on the chosen values in the viewModel and set it to calculatedIntensityLabel.Content
             //Todo (Make sure to convert the ChosenFrequency value to exahertz by multiplying it by 1.0E18)
-            calculatedIntensityLabel.Content = _viewModel.ChosenIntensity;
+            // Since it's likely to be a common conversion I added it to the constants rather than hardcode the conversion
+            calculatedIntensityLabel.Content = OpacityCalculatorConstants.OpacityDistanceFunction(
+                _viewModel.ChosenMaterialType,
+                _viewModel.ChosenIntensity,
+                _viewModel.ChosenFrequency * OpacityCalculatorConstants.ChosenFrequencyToExahertz,
+                _viewModel.ChosenDistance);
         }
 
         //Todo F. Modify this function to set an error message to errorMessage and return false if any of the input variables are outside of the ranges specified in OpacityCalculatorConstants
@@ -49,14 +54,14 @@ namespace CompassInterviewTest
             errorMessage = "";
             return true;
         }
-        
+
         private void PlotFrequencyButton_OnClick(object sender, RoutedEventArgs e)
         {
             //Todo G. Call ValidateInputVariables(out var errorMessage) and display error message to errorMessageLabel and return if input variable validation fails
             //Todo     Also use PlotView.Model = newPlotModel() to clear the plot if validation fails
             //Todo     Assign "N/A" to the calculatedIntensityLabel if validation fails
             //Todo     Make sure to clear the error message if there is no error
-                
+
             var newModel = new PlotModel()
             {
                 Title = $"Intensity through {_viewModel.ChosenDistance}m of {_viewModel.ChosenMaterialType.ToString()} across frequency range"
@@ -76,9 +81,17 @@ namespace CompassInterviewTest
             //Todo D. Similarly to how its done in PlotDistanceButton_OnClick, add a function series to the newModel variable
             //Todo     This function series will plot the calculated intensity from the minimum frequency to the maximum frequency, incrementing by the frequency increment constant. All of these are given the OpacityCalculatorConstants.cs
             //Todo     The function will use the input variable as frequency rather than the ChosenFrequency variable
-            
+
+            //Plot calculated intensity from the minimum frequency to the ChosenFrequency variable
+            newModel.Series.Add(
+                //Convert Chosen Frequency to exahertz
+                new FunctionSeries(x => OpacityCalculatorConstants.OpacityDistanceFunction(_viewModel.ChosenMaterialType, _viewModel.ChosenIntensity, x * 1.0E18, _viewModel.ChosenDistance),
+                OpacityCalculatorConstants.MinimumFrequency,
+                _viewModel.ChosenFrequency,
+                (_viewModel.ChosenFrequency - OpacityCalculatorConstants.MinimumFrequency) / OpacityCalculatorConstants.FrequencyIncrement));
+
             PlotView.Model = newModel;
-            
+
             UpdateCalculatedIntensity();
         }
         private void PlotDistanceButton_OnClick(object sender, RoutedEventArgs e)
@@ -87,7 +100,7 @@ namespace CompassInterviewTest
             //Todo     Also use PlotView.Model = newPlotModel() to clear the plot if validation fails
             //Todo     Assign "N/A" to the calculatedIntensityLabel if validation fails
             //Todo     Make sure to clear the error message if there is no error
-                
+
             var newModel = new PlotModel()
             {
                 Title = $"Intensity of {_viewModel.ChosenFrequency}EHz radiation through {_viewModel.ChosenMaterialType.ToString()} across to {_viewModel.ChosenDistance}m"
@@ -103,9 +116,9 @@ namespace CompassInterviewTest
                 TitleFontSize = 16,
             };
             newModel.Axes.Add(xAxis);
-            
+
             //Plot calculated intensity from the minimum distance to the ChosenDistance variable
-            newModel.Series.Add( 
+            newModel.Series.Add(
                 //Convert Chosen Frequency to exahertz
                 new FunctionSeries(x => OpacityCalculatorConstants.OpacityDistanceFunction(_viewModel.ChosenMaterialType, _viewModel.ChosenIntensity, _viewModel.ChosenFrequency * 1.0E18, x),
                 OpacityCalculatorConstants.MinimumDistance,
@@ -113,7 +126,7 @@ namespace CompassInterviewTest
                 (_viewModel.ChosenDistance - OpacityCalculatorConstants.MinimumDistance) / OpacityCalculatorConstants.DistanceIncrement));
 
             PlotView.Model = newModel;
-            
+
             UpdateCalculatedIntensity();
         }
     }
