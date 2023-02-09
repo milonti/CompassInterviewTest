61b93d4e6fa28da03160d46b3b221127b60d9dae
Michael Zipp <milonti1@gmail.com>
2023-02-09 10:33:03 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Wpf;
+using OxyPlot.Series;
+
+namespace CompassInterviewTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _viewModel;
+        
+        public MainWindow()
+        {
+            _viewModel = new MainWindowViewModel();
+
+            DataContext = _viewModel;
+            InitializeComponent();
+        }
+
+        private void PlotDistanceButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var newModel = new PlotModel()
+            {
+                Title = $"Intensity of {_viewModel.ChosenFrequency}EHz radiation through {_viewModel.ChosenMaterialType.ToString()} across to {_viewModel.ChosenDistance}m"
+            };
+            var xAxis = new LinearAxis
+            {
+                Title = "Distance",
+                Position = AxisPosition.Bottom,
+                IsZoomEnabled = false,
+                IsPanEnabled = false,
+                FontSize = 12,
+                TickStyle = TickStyle.Outside,
+                TitleFontSize = 16,
+            };
+            newModel.Axes.Add(xAxis);
+            
+            //Plot calculated intensity from the minimum distance to the ChosenDistance variable
+            newModel.Series.Add( 
+                //Convert Chosen Frequency to exahertz
+                new FunctionSeries(x => OpacityCalculatorConstants.OpacityDistanceFunction(_viewModel.ChosenMaterialType, _viewModel.ChosenIntensity, _viewModel.ChosenFrequency * 1.0E18, x),
+                OpacityCalculatorConstants.MinimumDistance,
+                _viewModel.ChosenDistance,
+                (_viewModel.ChosenDistance - OpacityCalculatorConstants.MinimumDistance) / OpacityCalculatorConstants.DistanceIncrement));
+
+            PlotView.Model = newModel;
+        }
+    }
+}
---
+++ OpacityCalculatorConstants.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CompassInterviewTest
+{
+    public class OpacityCalculatorConstants
+    {
+        public enum MaterialType
+        {
+            [Description("Air")]Air,
+            [Description("Water")]Water,
+            [Description("Carbon")]Carbon,
+            [Description("Alumninum")]Aluminum
+        }
+        
+        public const double MinimumDistance = 0;
+        public const double MaximumDistance = 1000;
+        public const double DistanceIncrement = 100.0;
+        
+        public const double MinimumIntensity = 1;
+        public const double MaximumIntensity = 1000;
+        
+        public const double MinimumFrequency = 2.41774826E19;
+        public const double MaximumFrequency = 1.20887413E20;
+        public const double FrequencyIncrement = 100.0;
+        
+        private const int ElectronVoltsA = 100000;
+        private const int ElectronVoltsB = 200000;
+        private const int ElectronVoltsC = 500000;
+        
+        //Linear Attenuation Coefficient Table (MaterialType, ElectronVolts) => LinearAttenuationCoefficient
+        private static Dictionary<(MaterialType, int), double> LinearAttenuationCoefficientTable = new Dictionary<(MaterialType, int), double>()
+        {
+            {(MaterialType.Air, ElectronVoltsA), 0.0195},
+            {(MaterialType.Air, ElectronVoltsB), 0.0159},
+            {(MaterialType.Air, ElectronVoltsC), 0.0112},
+            {(MaterialType.Water, ElectronVoltsA), 16.7},
+            {(MaterialType.Water, ElectronVoltsB), 13.6},
+            {(MaterialType.Water, ElectronVoltsC), 9.7},
+            {(MaterialType.Carbon, ElectronVoltsA), 33.5},
+            {(MaterialType.Carbon, ElectronVoltsB), 27.4},
+            {(MaterialType.Carbon, ElectronVoltsC), 19.6},
+            {(MaterialType.Aluminum, ElectronVoltsA), 43.5},
+            {(MaterialType.Aluminum, ElectronVoltsB), 32.4},
+            {(MaterialType.Aluminum, ElectronVoltsC), 22.7},
+        };
+
+        //Mass Attenuation Function
+        //v:  Frequency of light
+        private static readonly Func<MaterialType, double, double> LinearAttenuationFunction = (materialType, v) => LinearAttenuationCoefficientTable[(materialType, ElectronVoltsB)];
+        
+        //Opacity Distance Function
+        //i0: Intensity of light at initial point
+        //v:  Frequency
+        //x:  Distance from light source
+        public static Func<MaterialType, double, double, double, double> OpacityDistanceFunction = 
+            (materialType, i0, v, x) => i0 * Math.Exp(-1 * LinearAttenuationFunction(materialType, v) * x);
+    }
+}
