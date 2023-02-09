a5762820e0485b44d1f1ecca2a1988d438602b83
Michael Zipp <milonti1@gmail.com>
2023-02-09 11:37:02 -0500

    Added ToDo C

--- OpacityCalculatorConstants.cs
+++ OpacityCalculatorConstants.cs
@@ -8,34 +8,36 @@ namespace CompassInterviewTest
     {
         public enum MaterialType
         {
-            [Description("Air")]Air,
-            [Description("Water")]Water,
-            [Description("Carbon")]Carbon,
-            [Description("Alumninum")]Aluminum
+            [Description("Air")] Air,
+            [Description("Water")] Water,
+            [Description("Carbon")] Carbon,
+            [Description("Alumninum")] Aluminum
         }
-        
+
         private const double PlankConstant = 6.626E-34;
         private const double ElementalCharge = 1.602E-19;
-        
+
         //Frequency to electron volts
-        public static Func<double, double> FrequencyToElectronVolts = 
+        public static Func<double, double> FrequencyToElectronVolts =
             v => v * PlankConstant / ElementalCharge;
-        
+
         public const double MinimumDistance = 0;
         public const double MaximumDistance = 1000;
         public const double DistanceIncrement = 100.0;
-        
+
+        public const double ChosenFrequencyToExahertz = 1.0E18;
+
         public const double MinimumIntensity = 1;
         public const double MaximumIntensity = 1000;
-        
+
         public const double MinimumFrequency = 2.41774826E19;
         public const double MaximumFrequency = 1.20887413E20;
         public const double FrequencyIncrement = 100.0;
-        
+
         private const int ElectronVoltsA = 100000;
         private const int ElectronVoltsB = 200000;
         private const int ElectronVoltsC = 500000;
-        
+
         //Linear Attenuation Coefficient Table (MaterialType, ElectronVolts) => LinearAttenuationCoefficient
         private static Dictionary<(MaterialType, int), double> LinearAttenuationCoefficientTable = new Dictionary<(MaterialType, int), double>()
         {
@@ -52,7 +54,7 @@ namespace CompassInterviewTest
             {(MaterialType.Aluminum, ElectronVoltsB), 32.4},
             {(MaterialType.Aluminum, ElectronVoltsC), 22.7},
         };
-        
+
         //Todo E. Redefine this function to do the following:
         //Todo     1. Calculate and return the linearly approximated fx
         //Linear Interpolation Function
@@ -69,13 +71,28 @@ namespace CompassInterviewTest
         //Todo     4. Use LinearInterpolation Func to return an estimate of the LinearAttenuationCoefficient
         //Mass Attenuation Function
         //v:  Frequency of light
-        private static readonly Func<MaterialType, double, double> LinearAttenuationFunction = (materialType, v) => LinearAttenuationCoefficientTable[(materialType, ElectronVoltsB)];
-        
+        private static readonly Func<MaterialType, double, double> LinearAttenuationFunction(MaterialType materialType, double v)
+        {
+            double chosenVolts = FrequencyToElectronVolts(v);
+            double bottomVolt = ElectronVoltsA;  //Defaulting to the lowest lower bound
+            if (chosenVolts > ElectronVoltsB && chosenVolts < ElectronVoltsC) bottomVolt = ElectronVoltsB;
+            // Question, what do I do if it's greater than C? Not sure if that's possible given the real world values being used though
+
+            //Linear interpolation says lower bound to upper bound in parameter order
+            //So while I know the lower volt number, all the material coefficients are actually in reverse from that
+            //So I swapped their orders
+            if (bottomVolt == ElectronVoltsA)
+            {
+                return LinearInterpolation(LinearAttenuationCoefficientTable[materialType, ElectronVoltsB], LinearAttenuationCoefficientTable(materialType, ElectronVoltsA));
+            }
+            else return LinearInterpolation(LinearAttenuationCoefficientTable[materialType, ElectronVoltsC], LinearAttenuationCoefficientTable(materialType, ElectronVoltsB));
+        }
+
         //Opacity Distance Function
         //i0: Intensity of light at initial point
         //v:  Frequency
         //x:  Distance from light source
-        public static Func<MaterialType, double, double, double, double> OpacityDistanceFunction = 
+        public static Func<MaterialType, double, double, double, double> OpacityDistanceFunction =
             (materialType, i0, v, x) => i0 * Math.Exp(-1 * LinearAttenuationFunction(materialType, v) * x);
     }
 }
